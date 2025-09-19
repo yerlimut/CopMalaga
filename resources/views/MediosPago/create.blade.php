c76871b3c7a96632c766d16633b3ead2d679d794
Tequia <xenthrall0@gmail.com>
2025-09-19 08:38:51 -0500

    Se modifico la ruta de la carpeta layouts y se agregaron rutas a MediosPago

--- resources/views/MediosPago/create.blade.php
+++ resources/views/MediosPago/create.blade.php
@@ -11,21 +11,22 @@
 @section('Content')
 
     <div>
-        <form action="" class="d-flex justify-content-center align-items-center min-vh-100 " method="POST">
+        <form action="{{ route('mediospago.store') }}" class="d-flex justify-content-center align-items-center min-vh-100 " method="POST">
+            @csrf
 
             <div>
 
                 <img src="" alt="">
             </div>
 
-            <div class="d-fex flex-column form shadow">
-         
+            <div class="d-flex flex-column form shadow p-4">
+
                 <h2>Crear medio de pago</h2>
             
       
                 <div class="mb-3">
-                    <label for="medio_pago" class="form-label ">Medios de pago</label>
-                    <select class="form-select" name="medio_pago" id="medio_pago">
+                    <label for="nombre" class="form-label ">Medios de pago</label>
+                    <select class="form-select" name="nombre" id="nombre">
                         <option value="">Seleccione un medio de pago</option>
                         <option value="efectivo">Efectivo</option>
                         <option value="tarjeta_credito">Tarjeta de Crédito</option>
@@ -39,14 +40,14 @@
 
                 <div class="mb-4">
                     <label class="form-label">Descripción</label>
-                    <input class="form-control" type="text" name="descripcion" id="">
+                    <input class="form-control" type="text" name="descripcion" id="descripcion" placeholder="Ingrese una descripción">
                 </div>
 
                 <div>
 
-                
-              <a href="" class="btn btn-outline-dark">Volver</a>
-                    <Button type="submit">Crear</Button>
+
+              <a href="{{ route('mediospago.index') }}" class="btn btn-outline-dark">Volver</a>
+                    <Button type="submit" class="btn btn-outline-dark">Crear</Button>
                 </div>
             
           
