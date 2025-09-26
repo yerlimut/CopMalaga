22f77dc5305ed1540cb10e9b4950b8169ba9190b
Cristian Solano <cfsv2018@gmail.com>
2025-09-26 10:12:12 -0500

    Relacion Tipo Prestamo -> Prestamos

--- app/Models/TipoPrestamo.php
+++ app/Models/TipoPrestamo.php
@@ -12,4 +12,9 @@ class TipoPrestamo extends Model
         'descripcion',
         'interes'
     ];
+
+    public function prestamos ()
+    {
+        return $this->hasMany(Prestamos::class,'idTipoPrestamo');
+    }
 }
