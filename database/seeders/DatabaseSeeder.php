7df12d4bd2650efd973a2d7faa42e76048065d11
Cristian Solano <cfsv2018@gmail.com>
2025-09-26 10:57:05 -0500

    Funcion Run Prestamos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,5 +28,9 @@ class DatabaseSeeder extends Seeder
         $this->call([
             MedioPagoSeeder::class,
         ]);
+
+        $this->call([
+            PrestamosSeeder::class,
+        ]);
     }
 }
