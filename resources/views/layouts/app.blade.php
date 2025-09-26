932cdc258d5d108e4f3f37ca7ffdc92478651b40
Faiber Adrian Abril Alvarado <adrianfabril@gmail.com>
2025-09-26 08:38:16 -0500

    Revert "Manual de uso de AdminLte y archivo app.blade.php con plantilla de layout de AdminLte"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,17 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <title>@yield('title', 'Dashboard')</title>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/adminlte.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.11.0/styles/overlayscrollbars.min.css">
+    <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+
 </head>
 
-<body class="layout-fixed sidebar-expand-lg sidebar-open bg-body-tertiary">
+<body>
 
     <style>
         body {
@@ -21,7 +21,7 @@
 
         .iconBack {
             color: #298b29;
-            font-size: 24px;
+            font-size: 25px;
             transition: transform 0.1s ease-in;
         }
 
@@ -121,33 +121,31 @@
 
         }
     </style>
-    <div class="app-wrapper">
-        @include('Plantilla.header')
-        @include('Plantilla.menu')
 
-        <main class="app-main">
-            <div class="app-content-header">
-                <div class="container-fluid">
-                    @yield('titleContent')
-                </div>
-            </div>
 
-            <div class="app-content">
-                <div class="container-fluid">
-                    @yield('Content')
-                </div>
-            </div>
-        </main>
+    @yield('titleContent')
+
+    <div class="container">
+        <div class="row">
+            @yield('Content')
+        </div>
     </div>
 
 
-    <script
-        src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.11.0/browser/overlayscrollbars.browser.es6.min.js"></script>
+
+    <footer>
+
+    </footer>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.min.js"></script>
-    <script src="{{ asset('js/adminlte.js') }}"></script>
-    @yield('scripts')
+
+
 </body>
 
 </html>
